b65239fb3022084e2b48df9a42770787d88c661a
shakya-ctrl77 <shakyasanjay944@gmail.com>
2026-01-25 10:49:37 +0545

    pig game project

---
+++ pig.py
@@ -0,0 +1,57 @@
+import random
+
+def roll():
+    MIN_VALUE=1
+    MAX_VALUE=6
+    roll = random.randint(MIN_VALUE,MAX_VALUE)
+    
+    return roll
+while True:
+
+    players = input('Enter number of players (2-4):')
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between 2 - 4 players")
+
+    else:
+        print("Invalid, try again")
+
+print(players)  
+
+max_score = 50
+player_scores =[0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+    
+  for player_idx in range(players):
+    print("\nPlayer number", player_idx + 1,"turn has just started!")
+    print("Your total score is:", player_scores[player_idx],"\n")       
+    current_score = 0
+    
+    while True:
+        should_roll = input("would u like to roll(y)?")
+        if should_roll.lower() !="y":
+            break
+        
+        value= roll()
+        if value == 1:
+            print("you rolled a 1! Turn done!")
+            current_score = 0
+            break
+        else:
+            current_score += value
+            print("You rolled a:", value)
+            
+        print("your score is :", current_score)
+        
+    player_scores[player_idx] += current_score
+    print("Your total score is:", player_scores[player_idx])
+        
+              
+
+max_score = max(player_scores)
+winning_idx= player_scores.index(max_score)
+print("Player number", winning_idx + 1, "is the winner with a score of:", max_score )
